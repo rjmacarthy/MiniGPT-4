6a53fc56d5ebf33fc51716a0ef0a9072cb54a5ce
rjmacarthy <richardmacarthy@hotmail.com>
2023-05-24 15:02:46 +0100

    refactor api url

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+const apiUrl = 'http://localhost:8000'
+
+
 
 export const uploadFiles = async (files) => {
   const formData = new FormData();
   files.forEach((file) => {
     formData.append('images', file);
   });
-  const response = await axios.post('http://localhost:8000/upload', formData, {
+  const response = await axios.post(`${apiUrl}/upload`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
@@ -15,11 +18,11 @@ export const uploadFiles = async (files) => {
 }
 
 export const generate = async (prompt) => {
-  const response = await axios.post('http://localhost:8000/generate', { message: prompt });
+  const response = await axios.post(`${apiUrl}/generate`, { message: prompt });
   return response.data;
 }
 
 export const reset = async () => {
-  const response = await axios.post('http://localhost:8000/reset');
+  const response = await axios.post(`${apiUrl}/reset`);
   return response.data;
 }
